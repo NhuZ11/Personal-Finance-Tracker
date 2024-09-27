4ea604910d38250e8c4ad27e02261edec1074ae0
Nhuz11 <nhuzbajra59@gmail.com>
2024-09-27 23:54:36 +0545

    basic backend overview

--- pft/backend/index.js
+++ pft/backend/index.js
@@ -1,15 +1,35 @@
-const express = require('express');
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 const app = express();
-const PORT = 8000
-const dbConnect = require('./db')
+const PORT = 8000;
+const dbConnect = require("./db");
+var cors = require('cors')  //for cross origin resource sharing
+
+
+app.use(cors()) //using cors
+
+//for database connection
 dbConnect()
+  .then(() => {
+    app.listen(
+      (PORT || 8000,
+      () => {
+        console.log("Server is running on port ", PORT);
+      })
+    );
+  })
+  .catch((err) => {
+    console.log("Mongo db connection failed", err);
+  });
+
+
+
 // Middleware for parsing JSON
 app.use(express.json());
 
 // Basic route
-app.get('/', (req, res) => {
-  res.send('Hello, Node.js Backend!');
+app.get("/", (req, res) => {
+  res.send("Hello, Node.js Backend!");
 });
 
 // Start the server
